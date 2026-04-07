d599401112e2e2d8a1a930d437807f59c626e49c
httaX996 <damindakule@gmail.com>
2026-04-07 10:38:18 +0530

    Set bot credentials in config.py
    Updated configuration values for bot credentials.

--- bot/config.py
+++ bot/config.py
@@ -2,17 +2,17 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8594103311:AAHKqgzmEXZE-5Pt5FhJvLf8P_sja19ODyc"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "chuti"
 
-    API_ID = ""
+    API_ID = "21801369"
 
-    API_HASH = ""
+    API_HASH = "111a1d5da769f1b22a1e85114b0ea489"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "177667675708-b51vo5nsm39b31di0fords3q8ki5m9f3.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
     AUTH_USERS = [942731625]
 
